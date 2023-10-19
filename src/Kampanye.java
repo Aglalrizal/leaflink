df6cdfb7eb09178139bd433caf6b1488535a1999
ikeuherliana <herlianaikeu270@gmail.com>
2023-10-19 10:17:14 +0700

    update kelas kampanye

--- src/Kampanye.java
+++ src/Kampanye.java
@@ -5,9 +5,33 @@ public class Kampanye {
     private String idKampanye;
     private String nama;
     private String deskripsi;
-    private List<Person>volunteer = new ArrayList<>();
+    private List<Person> volunteer = new ArrayList<>();
     private String lokasi;
     private int targetPendanaan;
     private int sumbangan;
 
+    public Kampanye(String idKampanye, String nama, String deskripsi, String lokasi, int targetPendanaan) {
+        this.idKampanye = idKampanye;
+        this.nama = nama;
+        this.deskripsi = deskripsi;
+        this.lokasi = lokasi;
+        this.targetPendanaan = targetPendanaan;
+    }
+
+    public void tambahKampanye() {
+    }
+
+    public void tampilkanKampanye() {
+    }
+
+    public void editKampanye(String namaBaru, String deskripsiBaru, String lokasiBaru, int targetPendanaanBaru) {
+        this.nama = namaBaru;
+        this.deskripsi = deskripsiBaru;
+        this.lokasi = lokasiBaru;
+        this.targetPendanaan = targetPendanaanBaru;
+    }
+
+    public void tambahVolunteer(Person volunteer) {
+        this.volunteer.add(volunteer);
+    }
 }
